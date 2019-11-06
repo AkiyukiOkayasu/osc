fc96f8d3426a1398e4e93cd452fe936cc2de2d61
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2019-11-06 09:40:45 +0900

    ちょっと追加

--- gosc.go
+++ gosc.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	println("hoge")
+	fmt.Println(len("abc"))
+}
+
+func hoge(args ...interface{}) {
+	for _, arg := range args {
+		switch t := arg.(type) {
+		case int32:
+			fmt.Println("int32")
+
+		default:
+			fmt.Println("default")
+			fmt.Println(t)
+		}
+	}
 }
--- osc/osc.go
+++ osc/osc.go
@@ -1,15 +1,43 @@
 package osc
 
 import (
+	"bytes"
+	"bytes/bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"strconv"
 )
 
+// Client OSC送信オブジェクト
+type Client struct {
+	ip    string
+	port  int
+	laddr *net.UDPAddr
+}
+
+// CreateSender Sender作成関数
+func CreateSender(ip string, port int) *Client {
+	return &Client{ip: ip, port: port, laddr: nil}
+}
+
 // Send OSC送信関数
-func Send(ip string, port int, address string) {
-	portStr := strconv.Itoa(port)
-	udpRAddr, err := net.ResolveUDPAddr("udp", ip+":"+portStr)
+func (c *Client) Send(oscAddr string, args ...interface{}) error {
+	if oscAddr[0] != '/' {
+		fmt.Println("Error: OSC address")
+	}
+
+	data := new(bytes.Buffer)
+	oscArgs := new(bytes.Buffer)
+	typetags := []byte{','}
+
+	// OSCアドレス
+	data.WriteString(oscAddr)
+	data.WriteString('0')         //null文字
+	padneeded := len(oscAddr) % 4 //TODO 4バイト
+
+	portStr := strconv.Itoa(c.port)
+	udpRAddr, err := net.ResolveUDPAddr("udp", c.ip+":"+portStr)
 	if err != nil {
 		panic(err)
 	}
@@ -19,9 +47,22 @@ func Send(ip string, port int, address string) {
 	}
 	defer conn.Close()
 
-	if address[0] != '/' {
-		fmt.Println("Error: OSC address")
+	// typetag, osc arg
+	for _, arg := range args {
+		fmt.Println("%v", arg)
+		switch t := arg.(type) {
+		case int32:
+			typetags = append(typetags, 'i')
+			err := binary.Write(oscArgs, binary.BigEndian, int32(t))
+
+		case float32:
+			typetags = append(typetags, 'f')
+
+		case string:
+			typetags = append(typetags, 's')
+		}
 	}
 
 	fmt.Println("送信")
+	return nil
 }
