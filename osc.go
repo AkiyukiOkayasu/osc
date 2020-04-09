0eb6db9259f2724a53d22fd8ee040e0bafbf36b7
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2020-04-09 13:25:09 +0900

    Update: file split message.go

--- osc.go
+++ osc.go
@@ -9,7 +9,7 @@ import (
 	"fmt"
 	"net"
 	"strconv"
-	"strings"
+	"strings"
 )
 
 const nullChar = '\x00'
@@ -32,37 +32,6 @@ type Server struct {
 	laddr *net.UDPAddr
 }
 
-// Argument OSC argument
-type Argument struct {
-	typetag  rune
-	argument interface{}
-}
-
-// Message wraped OSC message
-type Message struct {
-	Address   string
-	arguments []Argument
-}
-
-// AddInt int追加
-func (m *Message) AddInt(arg int32) {
-	a := Argument{typetag: 'i', argument: arg}
-	m.arguments = append(m.arguments, a)
-}
-
-// AddFloat float追加
-func (m *Message) AddFloat(arg float32) {
-	a := Argument{typetag: 'f', argument: arg}
-	m.arguments = append(m.arguments, a)
-}
-
-// AddString string追加
-func (m *Message) AddString(arg string) {
-	arg = TerminateOSCString(arg)
-	a := Argument{typetag: 's', argument: arg}
-	m.arguments = append(m.arguments, a)
-}
-
 // NewSender Sender作成
 func NewSender(ip string, port int) *Client {
 	return &Client{ip: ip, port: port, laddr: nil}
@@ -71,37 +40,6 @@ func NewSender(ip string, port int) *Client {
 // NewReceiver Receiver作成
 func NewReceiver(port int) *Server {
 	return &Server{port: port, laddr: nil}
-}
-
-// Bytes get OSC typetag and argument in []byte
-func (m *Message) Bytes() []byte {
-	b := new(bytes.Buffer)
-	typetag := ","
-	for _, a := range m.arguments {
-		typetag += string(a.typetag)
-	}
-	typetag = TerminateOSCString(typetag)
-	b.WriteString(typetag)
-
-	for _, a := range m.arguments {
-		switch a.typetag {
-		case 'i':
-			if v, ok := a.argument.(int32); ok {
-				binary.Write(b, binary.BigEndian, v)
-			}
-		case 'f':
-			if v, ok := a.argument.(float32); ok {
-				binary.Write(b, binary.BigEndian, v)
-			}
-		case 's':
-			if v, ok := a.argument.(string); ok {
-				b.WriteString(v)
-			}
-		default:
-			println("Unexpected typetag")
-		}
-	}
-	return b.Bytes()
 }
 
 // Send OSC送信
